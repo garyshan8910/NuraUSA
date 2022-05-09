75754b32512c7323c06fba9c955c878a2f3ceb63
wen1 <jdwen1@gmail.com>
2022-05-08 21:34:40 -0700

    add assist_user table

--- nura/route.py
+++ nura/route.py
@@ -1,10 +1,12 @@
 from flask import render_template, redirect, url_for, request
 from flask_login import login_user, current_user, logout_user, login_required
-from nura.forms import LoginForm, RegisterForm
-from nura.models import Sysuser, So, Po
+from nura.forms import LoginForm, RegisterForm, SoForm
+from nura.models import AssistUser, So, Po
 from nura import db
 
 # 这里加装饰器，就会要求用户要登陆才能访问这个页面
+
+
 @login_required
 def index():
     posts = [
@@ -18,8 +20,8 @@ def index():
         },
         {
             'author': {'username': 'test1'},
-            'body': "hi I'm test1!"            
-        }      
+            'body': "hi I'm test1!"
+        }
     ]
     return render_template('index.html')
 
@@ -29,7 +31,7 @@ def login():
         return redirect(url_for('index'))
     form = LoginForm(csrf_enabled=False)
     if form.is_submitted():
-        u = Sysuser.query.filter_by(userName=form.username.data).first()
+        u = AssistUser.query.filter_by(username=form.username.data).first()
         if u is None:
             print('Invalid User Name')
             return redirect(url_for('login'))
@@ -47,35 +49,47 @@ def login():
         if next_page:
             return redirect(next_page)
         return redirect(url_for('index'))
-    return render_template('login.html', form = form)
+    return render_template('login.html', form=form)
+
 
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
 
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = RegisterForm()
     if form.validate_on_submit():
-        user = Sysuser(userName=form.username.data, email=form.email.data)
+        user = AssistUser(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
-        user.activeFlag = 1
+        user.active = 1
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('login'))
     return render_template('register.html', title='Registration', form=form)
 
+
 @login_required
 def so_po_mapping():
     return render_template('sopomapping.html')
 
-@login_required
-def so_details():
-    so_details = So.query
-    return render_template('sodetails.html', so_details=so_details)
 
 @login_required
+def so_details():
+    so_form = SoForm(request.form)
+    if request.method == "POST":
+        so_form.validate_on_submit()
+        # print(so_form.__dict__)
+        # print(request.method)
+        so_details = So.query.filter(So.num.contains(so_form.num.data)).all()
+    else:
+        so_details = So.query.all()
+    return render_template('sodetails.html', so_details=so_details, so_form=so_form)
+
+
+@ login_required
 def po_details():
     po_details = Po.query
     return render_template('podetails.html', po_details=po_details)
