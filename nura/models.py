7ab22570519bc3750d49d7716401eeddcdf1479e
Xiao Shan <xshan7@fb.com>
2022-05-09 13:19:59 +0800

    delete unused code

--- nura/models.py
+++ nura/models.py
@@ -44,21 +44,6 @@ class Sysuser(UserMixin, db.Model):
     customFields = db.Column(db.JSON)
     passwordLastModified = db.Column(db.DateTime)
 
-    def __repr__(self):
-        return 'id = {}, name = {} {}, userName = {}, userPwdHash = {}'.format(
-            self.id, self.firstName, self.lastName, self.userName, self.userPwd
-        )
-
-    def compute_md5_hash(self, password):
-        m = hashlib.md5()
-        m.update(password.encode('utf-8'))
-        return m.hexdigest()
-
-    def set_password(self, password):
-        self.userPwd = self.compute_md5_hash(password)
-
-    def check_password(self, password):
-        return self.userPwd == self.compute_md5_hash(password)
 
 # 获取用户是否登陆信息
 @login_manager.user_loader
